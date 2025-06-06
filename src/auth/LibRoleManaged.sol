e204380e001b41913e5cb7e78867d9c8c1c300d9
Kevin Kennis <kevinvkennis@gmail.com>
2025-06-06 07:38:57 -0700

    update licenses

--- src/auth/LibRoleManaged.sol
+++ src/auth/LibRoleManaged.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.25;
 
 import {RoleManager} from "./RoleManager.sol";
--- src/token/tRWA.sol
+++ src/token/tRWA.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.25;
 
 import {ERC4626} from "solady/tokens/ERC4626.sol";
